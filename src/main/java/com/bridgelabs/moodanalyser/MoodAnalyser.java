7b4515f75608a4967578d95293cbc041c8b11c8a
shro-2002 <96790181+shro-2002@users.noreply.github.com>
2023-12-04 18:32:37 +0530

    Added Refactored Code: Refactor the code to take the mood message in Constructor

--- src/main/java/com/bridgelabs/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/bridgelabs/moodanalyser/MoodAnalyser.java
@@ -2,6 +2,30 @@ package com.bridgelabs.moodanalyser;
 
 public class MoodAnalyser {
 
+	private String message;
+
+	/*
+	 * @Description- Default Constructor
+	 * 
+	 * @param- None
+	 * 
+	 * @return-None
+	 */
+	public MoodAnalyser() {
+		System.out.print("Default Constructor");
+	}
+
+	/*
+	 * @Description- Parameterized Constructor to initialize message
+	 * 
+	 * @param- String message
+	 * 
+	 * @return-None
+	 */
+	public MoodAnalyser(String message) {
+		this.message = message;
+	}
+
 	/*
 	 * @Description- This method is used to analyse mood of the user
 	 * 
@@ -10,7 +34,7 @@ public class MoodAnalyser {
 	 * @return-String mood
 	 */
 
-	public String analyseMood(String message) {
+	public String analyseMood() {
 		if (message.contains("Sad"))
 			return "SAD";
 		else
--- src/main/java/com/bridgelabs/moodanalyser/MoodAnalyserMain.java
+++ src/main/java/com/bridgelabs/moodanalyser/MoodAnalyserMain.java
@@ -5,16 +5,17 @@ public class MoodAnalyserMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Mood Analyser Program");
 
-		MoodAnalyser moodAnalyser = new MoodAnalyser();
+		MoodAnalyser moodAnalyser1 = new MoodAnalyser("I am in Sad Mood");
+		MoodAnalyser moodAnalyser2 = new MoodAnalyser("I am in Any Mood");
 
 		// TC-1.1
 
-		String mood = moodAnalyser.analyseMood("I am in Sad Mood");
+		String mood = moodAnalyser1.analyseMood();
 		System.out.println(mood);
 
 		// TC-1.2
 
-		String mood1 = moodAnalyser.analyseMood("I am in Any Mood");
+		String mood1 = moodAnalyser2.analyseMood();
 		System.out.println(mood1);
 	}
 
